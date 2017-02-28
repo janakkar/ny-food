ad17b07b734187a4f6e63e8aa11272c111ac11bf
velinovskav <velinovskav@gmail.com>
2017-02-28 22:04:13 +0100

    fix unit test for products component

--- src/app/product/products.component.spec.ts
+++ src/app/product/products.component.spec.ts
@@ -1,9 +1,17 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
+import {By, BrowserModule} from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { ProductsComponent } from './products.component';
+import {FormsModule} from "@angular/forms";
+import {ProductService} from "../domain/product.service";
+import {BadgeComponent} from "../common/badge.component";
+import {ProductListComponent} from "./product-list.component";
+import {ProductGradeComponent} from "./product-grade.component";
+import {AngularFireModule} from "angularfire2";
+import {firebaseConfig} from "../app.module";
+import {Calculator} from "../calculator/calculator";
 
 describe('ProductsComponent', () => {
     let component: ProductsComponent;
@@ -11,7 +19,9 @@ describe('ProductsComponent', () => {
 
     beforeEach(async(() => {
       TestBed.configureTestingModule({
-        declarations: [ ProductsComponent ]
+          declarations: [ ProductGradeComponent, ProductListComponent, BadgeComponent, ProductsComponent ],
+          imports: [ FormsModule, AngularFireModule.initializeApp(firebaseConfig) ],
+          providers: [ Calculator, ProductService ]
       })
       .compileComponents();
     }));
